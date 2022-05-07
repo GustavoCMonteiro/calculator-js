c5c584b4f46a7d4000069da5966091bd046bcbb4
GustavoCMonteiro <101683431+GustavoCMonteiro@users.noreply.github.com>
2022-05-07 19:44:44 -0300

    commit javascript

--- js/script.js
+++ js/script.js
@@ -1,23 +1,129 @@
 // variables of calc buttons
 const calcDisplay = document.getElementById("calc-display");
 const calcNumbers = document.querySelectorAll(".number");
-const calcSpecial = document.querySelectorAll(".special");
+const calcEquation = document.querySelectorAll(".equation");
+const calcEqual = document.querySelector(".equal");
+const calcClear = document.querySelector(".clear");
+const calcPlusMinus = document.querySelector(".plus-minus");
+const calcPercent = document.querySelector(".percent");
 
 // variables of other elements
 const checkBox = document.getElementById("checkbox");
 const bgBody = document.getElementById("bg-body");
 const calcBody = document.getElementById("calc-body");
+const textDark = document.querySelector(".text-dark");
 
 // function to change to dark mode
+checkBox.addEventListener("click", toggleDark);
+
 function toggleDark() {
   bgBody.classList.toggle("bg-dark");
   calcBody.classList.toggle("calc-bg-dark");
-  calcSpecial.forEach((i) => {
-    i.classList.toggle("sp-button-dark");
-  });
-  calcNumbers.forEach((i) => {
-    i.classList.toggle("button-dark");
-  });
+  calcDisplay.classList.toggle("button-dark");
+  calcNumbers.forEach((i) => i.classList.toggle("button-dark"));
+  calcEquation.forEach((i) => i.classList.toggle("sp-button-dark"));
+  calcEqual.classList.toggle("sp-button-dark");
+  calcClear.classList.toggle("sp-button-dark");
+  calcPlusMinus.classList.toggle("sp-button-dark");
+  textDark.classList.toggle("button-dark");
+  changeText();
 }
 
-checkBox.addEventListener("click", toggleDark);
+function changeText() {
+  if (textDark.textContent == "Dark Mode") {
+    textDark.textContent = "Light Mode";
+  } else {
+    textDark.textContent = "Dark Mode";
+  }
+}
+
+// calculator add number
+calcNumbers.forEach((button) =>
+  button.addEventListener("click", function () {
+    if (calcDisplay.textContent.length < 8) {
+      let tecla = button.textContent;
+      calcDisplay.textContent += tecla;
+    }
+  })
+);
+
+// calculator equations
+calcEquation.forEach((button) =>
+  button.addEventListener("click", function () {
+    let tecla = button.textContent;
+    if (tecla == "+") {
+      num1(+calcDisplay.textContent);
+      sinal("+");
+      calcDisplay.textContent = "";
+    } else if (tecla == "-") {
+      num1(+calcDisplay.textContent);
+      sinal("-");
+      calcDisplay.textContent = "";
+    } else if (tecla == "/") {
+      num1(+calcDisplay.textContent);
+      sinal("/");
+      calcDisplay.textContent = "";
+    } else if (tecla == "x") {
+      num1(+calcDisplay.textContent);
+      sinal("x");
+      calcDisplay.textContent = "";
+    } else if (tecla == "%") {
+      num1(+calcDisplay.textContent);
+      sinal("%");
+      calcDisplay.textContent = "";
+    }
+  })
+);
+
+let value1 = 0;
+let operator;
+let num1 = (value) => {
+  if (value == 0) {
+    value1 = 0;
+  } else {
+    value1 += value;
+  }
+};
+
+let sinal = (value) => (operator = value);
+
+function equation(value1, sinal, value2) {
+  if (sinal == "+") {
+    calcDisplay.textContent = parseFloat((value1 + value2).toFixed(5));
+  } else if (sinal == "-") {
+    calcDisplay.textContent = parseFloat((value1 - value2).toFixed(5));
+  } else if (sinal == "/") {
+    calcDisplay.textContent = parseFloat((value1 / value2).toFixed(5));
+  } else if (sinal == "x") {
+    calcDisplay.textContent = parseFloat((value1 * value2).toFixed(5));
+  } else if (sinal == "%") {
+    calcDisplay.textContent = parseFloat(((value2 * value1) / 100).toFixed(5));
+  }
+}
+
+// calculator Plus Minus change
+calcPlusMinus.addEventListener("click", () => {
+  let number = +calcDisplay.textContent;
+  if (Math.sign(number) == -1) {
+    calcDisplay.textContent = Math.abs(number);
+  } else {
+    calcDisplay.textContent = Math.abs(number) * -1;
+  }
+});
+
+// calculator equal sign
+calcEqual.addEventListener("click", () => {
+  if (calcDisplay.textContent == "") {
+    console.log("erro");
+  } else {
+    equation(value1, operator, +calcDisplay.textContent);
+    num1(0);
+  }
+});
+
+// calculator clear
+calcClear.addEventListener("click", () => {
+  calcDisplay.textContent = "";
+});
+
+console.log(Math.round(12.333));
